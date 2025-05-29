5bb5a9f294783983d9326410b5bde0f3f8d153a8
Troyano333 <eldiosapolo10@gmail.com>
2025-05-29 16:23:02 -0500

    c

--- php/conexion.php
+++ php/conexion.php
@@ -13,21 +13,6 @@ if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
-// Consultar fechas
-$consulta = $conexion->query("SELECT DISTINCT fecha FROM reservas");
-$fechas = [];
-
-// Almacenar las fechas obtenidas en un array
-while ($fila = $consulta->fetch_assoc()) {
-    $fechas[] = $fila['fecha'];
-}
-
-// Establecer el tipo de contenido a JSON
-header('Content-Type: application/json');
-
-// Enviar las fechas en formato JSON
-echo json_encode($fechas);
-
-// Cerrar la conexión
-$conexion->close();
+// Configurar la codificación de caracteres para evitar problemas con caracteres especiales
+$conexion->set_charset("utf8");
 ?>
